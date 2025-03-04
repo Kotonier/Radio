6ec306ac5628748d362f87bdfb19d30cd5e443e7
Kotonier <63809872+Kotonier@users.noreply.github.com>
2025-03-04 20:53:57 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -9,21 +9,12 @@ shared_script "fixDeleteVehicle.lua"
 shared_scripts {
     '@ox_lib/init.lua', 
     '@es_extended/imports.lua',
-    'config/**.lua',
 }
 
 client_scripts {
     'client/**.lua',
 }
 
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server/**.lua',
-}
-
-files {
-    'locales/*.json'
-}
 dependencies {
     'ox_lib'
 }
